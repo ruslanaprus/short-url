2a687d649784388480abbbd5951b43ad50ac320c
Maes-tr0 <nikita.opara.work@gmail.com>
2025-01-11 14:36:25 +0200

    Add User entity with validation

--- src/main/java/org/goit/urlshortener/model/User.java
+++ src/main/java/org/goit/urlshortener/model/User.java
@@ -1,4 +1,67 @@
 package org.goit.urlshortener.model;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@ToString(exclude = "urls")
+@Data
+@NoArgsConstructor
 public class User {
+    @Id
+    @SequenceGenerator(allocationSize = 1, name = "user_seq", sequenceName = "seq_user_id")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_seq")
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    @NotBlank(message = "Email cannot be blank")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @Column(nullable = false)
+    @Size(min = 8, message = "Password must be at least 8 characters long")
+    @NotBlank(message = "Password cannot be blank")
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,}$",
+            message = """
+                    Password must meet the following requirements:
+                    - At least 8 characters in length
+                    - At least one lowercase letter
+                    - At least one uppercase letter
+                    - At least one number
+                    """
+    )
+    private String password;
+
+
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Url> urls;
+
+    @PrePersist
+    protected void setCreationTimestamp() {
+        createdAt = LocalDateTime.now();
+    }
 }
