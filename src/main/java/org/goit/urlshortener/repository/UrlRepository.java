28fbc7a5193a0f71eca2241794f24047ae058f60
Anastasiia Izotova <22.nastia.22.enotik22@gmail.com>
2025-01-12 16:11:07 +0200

    Add findByShortCode method to UrlRepository

--- src/main/java/org/goit/urlshortener/repository/UrlRepository.java
+++ src/main/java/org/goit/urlshortener/repository/UrlRepository.java
@@ -1,4 +1,10 @@
 package org.goit.urlshortener.repository;
 
-public class UrlRepository {
+import org.goit.urlshortener.model.Url;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UrlRepository extends JpaRepository<Url, Long> {
+    Optional<Url> findByShortCode(String shortCode);
 }
