b8f9312bc9ba3df2f23f81dfd93e4d8df810c4f5
Maes-tr0 <nikita.opara.work@gmail.com>
2025-01-13 14:19:27 +0200

    Deleted all tests for user entity. Make changes in User

--- src/main/java/org/goit/urlshortener/model/User.java
+++ src/main/java/org/goit/urlshortener/model/User.java
@@ -10,8 +10,8 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Data;
@@ -38,10 +38,7 @@ public class User {
 
     @Column(nullable = false, unique = true)
     @NotBlank(message = "Email cannot be blank")
-    @Pattern(
-            regexp = "^[a-zA-Z0-9._%+-]+@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,}$",
-            message = "Invalid email format"
-    )
+    @Email
     private String email;
 
 
