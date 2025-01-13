c6f2ad847c7c5988e20e2e8b213170a1cada1175
Maes-tr0 <nikita.opara.work@gmail.com>
2025-01-13 13:15:07 +0200

    Fix entity user. Deleted tests for password

--- src/main/java/org/goit/urlshortener/model/User.java
+++ src/main/java/org/goit/urlshortener/model/User.java
@@ -13,7 +13,6 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -48,16 +47,6 @@ public class User {
 
     @Column(nullable = false)
     @NotBlank(message = "Password cannot be blank")
-    @Pattern(
-            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,}$",
-            message = """
-                    Password must meet the following requirements:
-                    - At least 8 characters in length
-                    - At least one lowercase letter
-                    - At least one uppercase letter
-                    - At least one number
-                    """
-    )
     private String password;
 
 
--- src/test/java/org/goit/urlshortener/model/user/UserValidationTest.java
+++ src/test/java/org/goit/urlshortener/model/user/UserValidationTest.java
@@ -15,15 +15,10 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class UserValidationTest {
-
     private static Validator validator;
 
     private static List<User> userWithInvalidEmails;
     private static List<User> userWithValidEmails;
-
-    private static List<User> userWithInvalidPasswords;
-    private static List<User> userWithValidPasswords;
-
 
     @BeforeAll
     public static void setUpValidator() {
@@ -53,26 +48,6 @@ class UserValidationTest {
                 User.builder().email("example_email@domain.info").password("12345Test").build(),
                 User.builder().email("admin@mailserver.com").password("12345Test").build()
         );
-
-        userWithInvalidPasswords = List.of(
-                User.builder().email("test@test.com").password("").build(),
-                User.builder().email("test@test.com").password("short1S").build(),
-                User.builder().email("test@test.com").password("alllowercase1").build(),
-                User.builder().email("test@test.com").password("ALLUPPERCASE1").build(),
-                User.builder().email("test@test.com").password("12345678").build(),
-                User.builder().email("test@test.com").password("Password").build(),
-                User.builder().email("test@test.com").password("Пароль123").build(),
-                User.builder().email("test@test.com").password("Password!@#").build()
-        );
-
-        userWithValidPasswords = List.of(
-                User.builder().email("test@test.com").password("Valid123").build(),
-                User.builder().email("test@test.com").password("Password123").build(),
-                User.builder().email("test@test.com").password("MySecurePassword1").build(),
-                User.builder().email("test@test.com").password("Abcdefg123").build(),
-                User.builder().email("test@test.com").password("Z1a2b3c4D").build(),
-                User.builder().email("test@test.com").password("Z1a2b3c4D#").build()
-        );
     }
 
     @Test
@@ -90,22 +65,4 @@ class UserValidationTest {
             assertTrue(violations.isEmpty(), "Expected no validation errors for email: " + user.getEmail());
         }
     }
-
-    @Test
-    void shouldRejectUser_WhenPasswordIsInvalid() {
-        for (User user : userWithInvalidPasswords) {
-            Set<ConstraintViolation<User>> violations = validator.validate(user);
-            assertFalse(violations.isEmpty(), "Expected validation error for password: " + user.getPassword());
-        }
-    }
-
-    @Test
-    void shouldValidateUser_WhenPasswordIsValid() {
-        for (User user : userWithValidPasswords) {
-            Set<ConstraintViolation<User>> violations = validator.validate(user);
-            assertTrue(violations.isEmpty(), "Expected no validation errors for password: " + user.getPassword());
-        }
-    }
-
-
 }
