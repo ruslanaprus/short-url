f6a4515d6fa643c1797cf58f5a1c0675f6006fa9
Anastasiia Izotova <22.nastia.22.enotik22@gmail.com>
2025-01-15 20:38:54 +0200

    Remove changes from UrlRepository

--- src/main/java/org/goit/urlshortener/repository/UrlRepository.java
+++ src/main/java/org/goit/urlshortener/repository/UrlRepository.java
@@ -1,10 +1,4 @@
 package org.goit.urlshortener.repository;
 
-import org.goit.urlshortener.model.Url;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface UrlRepository extends JpaRepository<Url, Long> {
-    Optional<Url> findByShortCode(String shortCode);
+public class UrlRepository {
 }
